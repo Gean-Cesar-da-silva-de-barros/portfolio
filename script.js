8f055a78b3317c5e02552a8ebdc170d70b2c7588
Gean_Dev <gean021216@gmail.com>
2026-03-15 19:26:51 -0300

    correcao no media queries mobile e tablet

--- script.js
+++ script.js
@@ -27,6 +27,7 @@ function menuShow() {
   if (menuMobile.classList.contains("open")) {
     menuMobile.classList.remove("open");
     document.querySelector(".icon").src = "imagens/menu_white_36dp.svg";
+    
   } else {
     menuMobile.classList.add("open");
     document.querySelector(".icon").src = "imagens/close_white_36dp.svg";
