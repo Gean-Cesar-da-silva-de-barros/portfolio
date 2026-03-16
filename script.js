0852eede503f6089c5616700c75505d96151ab57
Gean_Dev <gean021216@gmail.com>
2026-03-15 21:53:32 -0300

    adicionado script para troca de imagem ao clicar

--- script.js
+++ script.js
@@ -27,7 +27,6 @@ function menuShow() {
   if (menuMobile.classList.contains("open")) {
     menuMobile.classList.remove("open");
     document.querySelector(".icon").src = "imagens/menu_white_36dp.svg";
-    
   } else {
     menuMobile.classList.add("open");
     document.querySelector(".icon").src = "imagens/close_white_36dp.svg";
@@ -46,7 +45,7 @@ let mudarimg = document.querySelector("#mudar-tema img"); //pega a img do botao
 let body = document.querySelector("body"); //pega o body
 let SobreMim = document.querySelector(".SobreMim"); //pega a seção SobreMim
 let header = document.querySelector("header"); //pega o header
-
+//adiciona um evento de clique ao botao
 btn.addEventListener("click", function () {
   body.classList.toggle("dark-mode"); //adiciona ou remove a classe dark-mode do body
   SobreMim.classList.toggle("dark-mode"); //adiciona ou remove a classe dark-mode da seção SobreMim
@@ -91,18 +90,27 @@ function trocarImagemGmail() {
     : gmailBlack; //troca a img do gmail
 }
 function trocarImagemGitHub() {
-  const normal = "./imagens/githubclack50.png";
+  //função para trocar a imagem do github
+  const normal = "./imagens/githubclack50.png"; //
   const dark = "./imagens/githubwhite50.png";
   github.src = github.src.includes("githubclack50.png") ? dark : normal; //troca a img do github
 }
-
-function mudarFotoPrincipal() {}
-
+//efeito para os cards dos projetos
+//função para adicionar a classe de animação do animate.css ao card do projeto quando clicar nele
+let fotoPrincipalImg = document.querySelector(".fotoPrincipal img");
+let mudarImg = document.querySelector(".mudar-img"); //pega o botão de mudança de imagem
+mudarImg.addEventListener("click", () => {
+  if (fotoPrincipalImg.src.includes("fotoPrincipalBlack.jpg")) {
+    fotoPrincipalImg.src = "./imagens/fotoPrincipalWhite.png";
+  } else {
+    fotoPrincipalImg.src = "./imagens/fotoPrincipalBlack.jpg";
+  }
+});
 function fliparProjeto(id) {
-  let card = document.getElementById(id);
-  card.classList.add("animate__animated", "animate__shakeX");
+  let card = document.getElementById(id); //pega o card pelo id
+  card.classList.add("animate__animated", "animate__jello"); //adiciona as classes de animação do animate.css
   setTimeout(() => {
-    card.classList.remove("animate__animated", "animate__shakeX");
+    card.classList.remove("animate__animated", "animate__jello");
   }, 800);
 }
 
@@ -110,13 +118,22 @@ function fliparProjeto(id) {
 window.addEventListener("scroll", function () {
   let header = document.querySelector("#header");
   let myname = document.querySelector("#myname");
-
-  if (window.scrollY > 300) {
-    header.classList.add("rolagem");
-
-    myname.textContent = "Gean Cesar";
+  //botao de subir aparecer quando rolar a página
+  let btnSubir = document.querySelector(".btnSubir");
+  if (window.scrollY > 500) {
+    //se a posição do scroll for maior que 500px
+    btnSubir.style.display = "block"; //mostra o botão de subir
   } else {
-    header.classList.remove("rolagem");
-    myname.textContent = "Portfólio";
+    //se não, esconde o botão de subir
+    btnSubir.style.display = "none"; //esconde o botão de subir
+  }
+  //header ficar mais escuro e com borda quando rolar a página
+  if (window.scrollY > 300) {
+    header.classList.add("rolagem"); //adiciona a classe rolagem ao header
+
+    myname.textContent = "Gean Cesar"; //troca o texto do myname para "Gean Cesar"
+  } else {
+    header.classList.remove("rolagem"); // remove a classe rolagem do header
+    myname.textContent = "Portfólio"; //troca o texto do myname para "Portfólio"
   }
 });
